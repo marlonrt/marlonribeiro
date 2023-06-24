1a458aa6db38526b2dfc1a8f9161633cba8cfba3
Marlon Ribeiro <119014294+marlonrt@users.noreply.github.com>
2023-06-24 18:11:00 -0300

    fix: Play/Pause

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function initAudio() {
     onloaderror: function () {
       console.error('Erro ao carregar o arquivo de áudio:', playlist[currentSongIndex]);
     },
-    volume: 1.0
+    volume: 1.0 // Definir volume inicial (opcional)
   });
 
   // Obter o nome do arquivo sem a extensão e com a primeira letra maiúscula
@@ -29,17 +29,20 @@ function initAudio() {
     fileNameWithoutExtension.slice(1);
 
   document.getElementById("currentSongName").textContent = formattedSongName;
+
+  document.getElementById("playPauseIcon").classList.remove("fa-pause");
+  document.getElementById("playPauseIcon").classList.add("fa-play");
 }
 
 function togglePlayPause() {
-  if (audio.playing()) {
-    audio.pause();
-    document.getElementById("playPauseIcon").classList.remove("fa-pause");
-    document.getElementById("playPauseIcon").classList.add("fa-play");
-  } else {
+  if (!audio.playing()) {
     audio.play();
     document.getElementById("playPauseIcon").classList.remove("fa-play");
     document.getElementById("playPauseIcon").classList.add("fa-pause");
+  } else {
+    audio.pause();
+    document.getElementById("playPauseIcon").classList.remove("fa-pause");
+    document.getElementById("playPauseIcon").classList.add("fa-play");
   }
 }
 
@@ -51,51 +54,43 @@ function playNext() {
   audio.stop();
   audio = null;
   initAudio();
-  document.getElementById("playPauseIcon").classList.remove("fa-play");
-  document.getElementById("playPauseIcon").classList.add("fa-pause");
+  
+  var playPauseIcon = document.getElementById("playPauseIcon");
+  if (playPauseIcon.classList.contains("fa-play")) {
+    playPauseIcon.classList.remove("fa-play");
+    playPauseIcon.classList.add("fa-pause");
+  }
+  
+  audio.play(); // Play the next song automatically
 }
 
+
+
 window.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("playPauseIcon").addEventListener("click", function () {
+    togglePlayPause();
+  });
+
+  document.getElementById("nextSongIcon").addEventListener("click", function () {
+    playNext();
+  });
+
+  var volumeRange = document.getElementById("volumeRange");
+  var audioVolume = document.getElementById("audioVolume");
+
+  volumeRange.addEventListener("input", function () {
+    var volume = parseFloat(volumeRange.value / 100);
+    audio.volume(volume);
+
+    // Atualizar o ícone de volume com base no valor do slider
+    if (volume === 0) {
+      audioVolume.className = "fas fa-volume-mute";
+    } else if (volume < 0.5) {
+      audioVolume.className = "fas fa-volume-down";
+    } else {
+      audioVolume.className = "fas fa-volume-up";
+    }
+  });
+
   initAudio();
 });
-
-document.getElementById("playPauseIcon").addEventListener("click", function () {
-  togglePlayPause();
-});
-
-document.getElementById("nextSongIcon").addEventListener("click", function () {
-  playNext();
-});
-
-var volumeRange = document.getElementById("volumeRange");
-var audioVolume = document.getElementById("audioVolume");
-
-volumeRange.addEventListener("input", function () {
-  var volume = parseFloat(volumeRange.value / 100);
-  audio.volume(volume);
-
-  // Atualizar o ícone de volume com base no valor do slider
-  if (volume === 0) {
-    audioVolume.className = "fas fa-volume-mute";
-  } else if (volume < 0.5) {
-    audioVolume.className = "fas fa-volume-down";
-  } else {
-    audioVolume.className = "fas fa-volume-up";
-  }
-});
-
-
-var audioControls = document.getElementById("audioControls");
-var prevScrollPos = window.pageYOffset;
-
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-
-  if (prevScrollPos > currentScrollPos) {
-    audioControls.style.opacity = "1";
-  } else {
-    audioControls.style.opacity = "0";
-  }
-
-  prevScrollPos = currentScrollPos;
-};
